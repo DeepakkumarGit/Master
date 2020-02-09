b842715401259810b0ef820b8a9edbfc2734dc43
Muthu <muthu.k@tringapps.com>
2020-02-09 14:11:11 +0530

    Added constructor

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React,{Component} from 'react'
-import {View,Text} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
 export default class app extends Component {
-render() {
-return(
-    <View>
-    <Text style = {{fontSize : 20,color : green}}>Hello world</Text>
-    <Text style = {{fontSize : 20,color : green}}>Student</Text>
-    </View>
-) 
-}
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <View>
+                <Text style={{ fontSize: 20, color: green }}>Hello world</Text>
+                <Text style={{ fontSize: 20, color: green }}>Student</Text>
+            </View>
+        )
+    }
 }
