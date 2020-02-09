d2b5d7d03c05543635d059e3d712750b00fa9b6f
Muthu <muthu.k@tringapps.com>
2020-02-09 14:14:35 +0530

    Fint style changes

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import {View,Text} from 'react-native'
 
 export default class app extends Component {
 render() {
+
 return(
     <View>
-    <Text style = {{fontSize : 20,color : green}}>Hello world</Text>
-    <Text style = {{fontSize : 20,color : green}}>Student</Text>
+    <Text style = {{fontSize : 25,color : green}}>Hello world</Text>
+    <Text style = {{fontSize : 25,color : green}}>Student</Text>
     </View>
 ) 
 }
