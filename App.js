567083dd19148b16accc73e8f9d2e5471ab25871
Muthu <muthu.k@tringapps.com>
2020-02-09 14:04:58 +0530

    Completed hello world project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+export default class app extends Component {
+render() {
+return(
+    <View>
+    <Text style = {{fontSize : 20,color : green}}>Hello world</Text>
+    </View>
+) 
+}
+}
